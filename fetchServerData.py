04b92189b2f17590ee64e0c6fbb590d1b4c52af7
DaNious <xz.song1993@gmail.com>
2023-04-23 11:18:58 +0800

    fetch data and save to file

--- fetchServerData.py
+++ fetchServerData.py
@@ -2,6 +2,10 @@ import paho.mqtt.client as mqtt
 import json
 import time
 import csv
+from datetime import datetime
+import os
+
+from serverMsg import serverMsg
 
 HOST = '39.107.248.79'
 
@@ -12,19 +16,13 @@ def on_connect(client, userdata, flags, rc):
     print('Connected with result code '+str(rc))
 # On_message callback
 def on_message(client, userdata, msg):
-    msgStr = msg.payload.decode("utf-8")
-    msgModal = msg.topic
-    dataStr = msgStr[msgStr.find("[")+1:msgStr.find("]")]
-    dataList = list(dataStr.split(","))
-    file = open(msgModal[msgModal.rfind("/")+1:]+"_data.csv", "a")
-    # csv_writer = csv.writer(file, delimiter=",")
-    for data in dataList:
-        if data != "":
-            # print(type(data))
-            print(data)
-            file.write(data+",\n")
-    print("Write Acc Data")
-    file.close()
+    dataMsg = serverMsg(msg.topic, msg.payload)
+    data = dataMsg.fetchData()
+    # save to a local file
+    currentDateTime = datetime.now().strftime("%Y%m%d_%H_%M")
+    if not os.path.exists("data/"+currentDateTime):
+        os.makedirs("data/"+currentDateTime)
+    dataMsg.saveData("data/"+currentDateTime, data)
 
 client = mqtt.Client()
 
@@ -38,9 +36,9 @@ client.connect(HOST, PORT, 60)
 # Fetch data (comment as needed)
 # Raw data
 # client.subscribe("d/dev-sensing/4049712210/raw/ppg", 0)
-# client.subscribe("d/dev-sensing/4049712210/raw/gsr", 0)
-client.subscribe("d/dev-sensing/4049712210/raw/acc", 0)
-# client.subscribe("d/dev-sensing/4049712210/raw/gyro", 0)
+client.subscribe("d/dev-sensing/4049712210/raw/gsr", 0)
+# client.subscribe("d/dev-sensing/4049712210/raw/acc", 0)
+#client.subscribe("d/dev-sensing/4049712210/raw/gyro", 0)
 # client.subscribe("d/dev-sensing/4049712210/raw/env", 0)
 # Feature data
 # client.subscribe("d/dev-sensing/4049712210/feature/ppg", 0)
---
+++ serverMsg.py
@@ -0,0 +1,19 @@
+class serverMsg:
+    def __init__(self, topic, payload):
+        self.topic = topic
+        self.payload = payload.decode("utf-8")
+    
+    def fetchData(self):
+        # data are strings between every "[]"
+        dataStr = self.payload[self.payload.find("[")+1:self.payload.find("]")]
+        dataList = list(dataStr.split(","))
+        return dataList
+    
+    def saveData(self, dir, dataList):
+        # file name is directory/sensing modality
+        with open(dir+"/"+self.topic[self.topic.rfind("/")+1:]+"_data.csv", "a+") as file: 
+            for i in range(4):
+                print("Write data to file(s)"+"."*i)
+            for data in dataList:
+                if data != "":
+                    file.write(data+",\n")
